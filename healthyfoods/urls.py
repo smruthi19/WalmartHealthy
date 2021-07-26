8d25d795e13545b9000aa8c062d163f2b62337fe
Smruthi <mail2smruthi@gmail.com>
2021-07-26 11:56:08 -0400

    second

--- healthyfoods/urls.py
+++ healthyfoods/urls.py
@@ -18,6 +18,7 @@ from myapp import views
 from django.urls import path
 from django.conf.urls import url
 from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
